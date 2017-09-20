b4d762ce2c4d562b92ed6a4f4f20bb6a14678ac2
Eduardo Escalera <eduardo.escalera@ponderalab.com>
2017-09-20 01:45:07 -0500

    basic app for uploading

--- mapa/urls.py
+++ mapa/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
         url(r'^$', views.index),
+        url(r'^mapa/', TemplateView.as_view(template_name="mapa/mapa.html"),
+                   name='mapa'),
     ]
