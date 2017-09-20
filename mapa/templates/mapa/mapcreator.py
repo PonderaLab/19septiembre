99e655ef943aa1ad86cecd9bd09c8231546c5a01
Eduardo Escalera <eduardo.escalera@ponderalab.com>
2017-09-20 13:40:04 -0500

    urgente gruas

--- mapa/templates/mapa/mapcreator.py
+++ mapa/templates/mapa/mapcreator.py
@@ -43,6 +43,8 @@ for id, r in tmp_df.iterrows():
         color = 'green'
     elif r['id'] == 'AcopioN':
         color = 'black'
+    elif r['id'] == 'Urgente':
+        color = 'white'
 
     else:
         color = 'white'
