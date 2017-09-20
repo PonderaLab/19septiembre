ee66a37a9c174c298c825ad8003b3a305ed69f8c
Eduardo Escalera <eduardo.escalera@ponderalab.com>
2017-09-20 03:00:00 -0500

    __mapcreator__ pulls real data

--- mapa/templates/mapa/mapcreator.py
+++ mapa/templates/mapa/mapcreator.py
@@ -7,7 +7,7 @@ import geopandas as gpd
 
 tmp_df = pd.read_csv('db_all_db.csv', dtype={
                      'store_point': str, 'geometry': str})
-tmp_df.geometry = tmp_df.geometry.map(lambda x: loads(x))
+#tmp_df.geometry = tmp_df.geometry.map(lambda x: loads(x))
 
 
 #tmp_df.store_point = tmp_df.store_point.map(lambda x: loads(x))
@@ -15,17 +15,6 @@ tmp_df.geometry = tmp_df.geometry.map(lambda x: loads(x))
 #tmp_df = tmp_df.drop('store_point', axis=1)
 #crs = {'init': 'epsg:4326'}
 #xolo_gdf = gpd.GeoDataFrame(tmp_df, crs=crs, geometry=tmp_df.geometry)
-
-tmp_df['style'] = [
-    {'fillColor': '#1f77b4', 'weight': 1, 'color': '#1f77b4', 'fillOpacity': 0.3},
-    {'fillColor': '#ff7f0e', 'weight': 1, 'color': '#ff7f0e', 'fillOpacity': 0.3},
-    {'fillColor': '#2ca02c', 'weight': 1, 'color': '#2ca02c', 'fillOpacity': 0.3},
-    {'fillColor': '#d62728', 'weight': 1, 'color': '#d62728', 'fillOpacity': 0.3},
-    {'fillColor': '#9467bd', 'weight': 1, 'color': '#9467bd', 'fillOpacity': 0.3},
-    {'fillColor': '#17becf', 'weight': 1, 'color': '#17becf', 'fillOpacity': 0.3},
-    {'fillColor': '#bcbd22', 'weight': 1, 'color': '#bcbd22', 'fillOpacity': 0.3},
-    {'fillColor': '#c81818', 'weight': 1, 'color': '#9467bd', 'fillOpacity': 0.3},
-]
 
 crs = {'init': 'epsg:4326'}
 
