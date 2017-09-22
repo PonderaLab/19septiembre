a0aaf7b683231b6a15ee06acf266b7d7b88fa0b4
Aldo Juarez Acevedo <aldo.juarez@ciencias.unam.mx>
2017-09-21 23:18:29 -0500

    __bucle.py__ fixed errors

--- bucle.py
+++ bucle.py
@@ -6,15 +6,18 @@ while True:
 
  try:
 
-      args1 = sp.call("git add mapa.html",shell=True)
-      args2 = sp.call("git commit -m 'autocommit mapa.html' ",shell=True)
-      dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-      args3 = sp.call("git push origin master",shell=True)
-      args4 = sp.call("git pull",shell=True)
+ 	sp.call("python mapcreator.py", shell=True)
+ 	delay(1200000)
+    sp.call("git add mapa.html",shell=True)
+    delay(600000)
+    sp.call("git commit -m 'autocommit mapa.html "+dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"' ",shell=True)
+    delay(600000)
+    sp.call("git push origin master",shell=True)
+      
 
      
 
-        delay(1800000000)
+        
 
 
 
